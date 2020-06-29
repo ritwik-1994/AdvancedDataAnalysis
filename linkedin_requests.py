5c128cca31706bbb8af8c61e5b30da75187607d8
ritwik-1994 <59331835+ritwik-1994@users.noreply.github.com>
2020-06-30 00:07:57 +0530

    Update linkedin_requests.py

--- linkedin_requests.py
+++ linkedin_requests.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.keys import Keys
 import pyautogui as pag
 import time
 
+#Logging intoo Linkedin
 def login():
 
 	driver.find_element_by_class_name('main__sign-in-link').click()
@@ -13,7 +14,7 @@ def login():
 	time.sleep(1)
 	password_input.send_keys("Enter-Password")
 	driver.find_element_by_xpath('//button[text()="Sign in"]').click()
-
+#Redirecting to My Network
 def goto_network():
 	try:
 		driver.find_element_by_id("mynetwork-tab-icon").click()
@@ -22,7 +23,7 @@ def goto_network():
 		body = driver.find_element_by_tag_name("body")
 		body.send_keys(Keys.CONTROL +'t')
 		driver.get(nurl)
-
+#Using PyAutoGUI to click repeatedly
 def get_details():
 	for i in range(0,30):
 		time.sleep(5)		
